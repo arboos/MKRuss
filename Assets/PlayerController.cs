cc455fe3d0ec4e1aee12213e561ee19cf3a7c192
arboos <151074202+arboos@users.noreply.github.com>
2024-03-27 21:27:15 +0300

    Multiple Character Controller added
    Added functions: jump, walk to multiple persons

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public Vector2 movementInput;
+    public int id;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+        if (movementInput.y > 0.2f && isGrounded()) Jump();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(movementInput.x * speed * Time.deltaTime, rb.velocity.y);
+    }
+    
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 1f)) return true;
+        return false;
+    }
+
+    public void OnMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+}
