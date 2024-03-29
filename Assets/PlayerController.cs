bcafcd4d69625c68755735c362274be6ba813ea8
arboos <151074202+arboos@users.noreply.github.com>
2024-03-29 19:58:09 +0300

    Walk animations for player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,19 +10,23 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public Vector2 movementInput;
     public int id;
-    
-    
+
+    [SerializeField] private Transform groundPos;
+
+
     private Rigidbody rb;
+    private Animator animator;
     private GameObject opponent;
-    
-    
+
+
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         GameObject[] opponents = GameObject.FindGameObjectsWithTag("Player");
         foreach (var character in opponents)
         {
-            if(character.GetComponent<PlayerController>().id == id) continue;
+            if (character.GetComponent<PlayerController>().id == id) continue;
             opponent = character;
             return;
         }
@@ -32,23 +36,30 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         if (movementInput.y > 0.2f && isGrounded()) Jump();
+        print(isGrounded());
+        animator.SetFloat("velocityY", rb.velocity.y);
     }
 
     private void Move()
     {
         rb.velocity = new Vector2(movementInput.x * speed * Time.deltaTime, rb.velocity.y);
     }
-    
+
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        animator.SetTrigger("Jump");
     }
 
     private bool isGrounded()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, 1f)) return true;
+        if (Physics.Raycast(groundPos.position, Vector3.down, 0.1f)) return true;
         return false;
     }
 
-    public void OnMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+        animator.SetFloat("movementX", movementInput.x);
+    }
 }
